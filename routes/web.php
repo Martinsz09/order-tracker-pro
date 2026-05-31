9cbb831be72fc174925232101087bee54a574614
Martinsz09 <mateusbelmiro09@gmail.com>
2026-05-31 14:19:58 -0300

    update routes

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,18 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DispatchController;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return redirect('/login');
+});
+
 Route::middleware('auth')->group(function () {
+    Route::get("/", function () {
+        return Redirect("/dashboard");
+    });
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
